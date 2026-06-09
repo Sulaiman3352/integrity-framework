097f8195587cfd9ed909abe77e733c77cb021d74
Sulaiman3352 <Sulaiman3352@gmail.com>
2026-06-09 20:05:22 +0300

    submit to ring buffer

--- kern/execve.bpf.c
+++ kern/execve.bpf.c
@@ -21,3 +21,23 @@ struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);   // "what kind of map?" → a ring buffer
     __uint(max_entries, 256 * 1024);      // "how big?"          → 256 KB
 } events SEC(".maps");                    // name it "events", put it in the maps section
+struct event *unused_event __attribute__((unused)); // the perpose of this is to force the compiler to acknowledge struct event as a used type → it gets preserved in the BTF → bpf2go finds it → generates the Go struct automatically.
+
+SEC("tracepoint/syscalls/sys_enter_execve")
+int handle_execve(struct trace_event_raw_sys_enter *ctx){
+    struct event *e;
+    e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+
+    if (!e)
+        return 0;
+    e->pid = bpf_get_current_pid_tgid() >> 32;
+    e->uid = bpf_get_current_uid_gid() & 0xffffffff;
+    bpf_get_current_comm(&e->comm, sizeof(e->comm));
+
+    const char *filename = (const char *)ctx->args[0];
+    bpf_probe_read_user_str(&e->filename, sizeof(e->filename), filename);
+    bpf_ringbuf_submit(e, 0);
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
