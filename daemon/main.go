891270f46970841d4d7fb2e3850064b8c5244c36
Sulaiman3352 <Sulaiman3352@gmail.com>
2026-06-11 20:42:52 +0300

    Open a Reader on The Ring Buffer

--- daemon/main.go
+++ daemon/main.go
@@ -2,20 +2,47 @@ package main
 
 import (
 	"log"
+	"encoding/binary"
 
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/ringbuf"
 	"github.com/cilium/ebpf/rlimit"
 )
 
 func main() {
-
+	// Remove The Memlock Limit
 	if err := rlimit.RemoveMemlock(); err != nil {
-		log.Fatalf("something failed: %v", err)
+		log.Fatalf("failed to remove Memlock limit: %v", err)
 	}
 
+	// Load The eBPF Objects Into The Kernel
 	var objs bpfObjects
-
 	if err := loadBpfObjects(&objs, nil); err != nil {
 		log.Fatalf("failed to load eBPF objects: %v", err)
 	}
 	defer objs.Close()
+
+	// Attach The Probe To The Tracepoint(hook)
+	tp, err := link.Tracepoint("syscalls", "sys_enter_execve", objs.HandleExecve, nil)
+	if err != nil {
+		log.Fatalf("failed to load Probe to tracepoint: %v", err)
+	}
+	defer tp.Close()
+
+	// Open a Reader on The Ring Buffer
+	rd, err := ringbuf.NewReader(objs.Events)
+	if err != nil {
+		log.Fatalf("failed to read the events: %v", err)
+	}
+	defer rd.Close()
+
+	//
+	for {
+		record, err := rd.Read()
+		if err != nil {
+			log.Fatalf("The reader got closed")
+		}
+		var event bpfEvent
+
+	}
 }
